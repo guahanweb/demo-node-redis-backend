956a48158274013f590944885f1d0c53613e547d
Garth Henson <garth@guahanweb.com>
2021-07-27 15:13:18 -0400

    adding redis support and boilerplate express listener

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,29 @@
-console.log("hello, world!");
+import config from "./config"
+import express from "express"
+import { getLogger } from "./lib/logger"
+import { connect as initializeRedis } from "./lib/redis"
+
+const logger = getLogger("main", config.app);
+const app = express();
+
+// execute the main program
+logger.debug("starting app");
+main();
+
+async function main() {
+	// bootstrap any necessary connections
+	await bootstrap(app);
+	logger.debug("bootstrap completed");
+
+	// start listening for traffic
+	app.listen(config.app.port, function () {
+		logger.info(`listening on port: ${config.app.port}`);
+	})
+}
+
+async function bootstrap(app: Express.Application) {
+	// initialize redis connection
+	const redisLogger = getLogger("redis", config.app);
+	await initializeRedis(config.redis, redisLogger);
+	logger.debug("redis initialized");
+}
